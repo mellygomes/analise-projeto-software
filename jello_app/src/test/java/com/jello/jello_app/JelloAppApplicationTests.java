5fe60b7d251a0f16037c3b2bda172c3b2d1445b6
jupitters <83190205+jupitters@users.noreply.github.com>
2026-04-08 18:06:08 -0300

    Add files via upload

---
+++ jello_app/src/test/java/com/jello/jello_app/JelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jello.jello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
