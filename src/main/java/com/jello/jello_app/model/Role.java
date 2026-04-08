c9c177f954137578c8d55405363ceab8f12be87f
jupitters <sampaiobale@gmail.com>
2026-04-08 20:55:12 -0300

    data initializer

--- src/main/java/com/jello/jello_app/model/Role.java
+++ src/main/java/com/jello/jello_app/model/Role.java
@@ -2,12 +2,14 @@ package com.jello.jello_app.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Collection;
 import java.util.HashSet;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
