3d2bf7e4ba39ddc8d45bbcefae1685da57ae8569
jupitters <sampaiobale@gmail.com>
2026-04-08 19:29:13 -0300

    utils generate token

--- src/main/java/com/jello/jello_app/security/jwt/JwtUtils.java
+++ src/main/java/com/jello/jello_app/security/jwt/JwtUtils.java
@@ -1,11 +1,16 @@
 package com.jello.jello_app.security.jwt;
 
 import com.jello.jello_app.security.user.AppUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -16,13 +21,23 @@ public class JwtUtils {
     @Value("${auth.token.expirationInMils}")
     private Integer expirationTime;
 
-    public String generateTokenForUser(Authentication authenticaition) {
-        AppUserDetails userPrincipal = (AppUserDetails) authenticaition.getPrincipal();
+    public String generateTokenForUser(Authentication authentication) {
+        AppUserDetails userPrincipal = (AppUserDetails) authentication.getPrincipal();
 
         List<String> roles = userPrincipal.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority).toList();
 
-        return Jwts
+        return Jwts.builder()
+                .subject(userPrincipal.getUsername())
+                .claim("roles", roles)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(key())
+                .compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 }
