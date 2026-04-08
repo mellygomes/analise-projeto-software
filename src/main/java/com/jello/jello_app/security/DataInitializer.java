73a37124bf1a7034bedde767cbb05f4eb0bed037
jupitters <sampaiobale@gmail.com>
2026-04-08 20:52:30 -0300

    data initializer

---
+++ src/main/java/com/jello/jello_app/security/DataInitializer.java
@@ -0,0 +1,40 @@
+package com.jello.jello_app.security;
+
+import com.jello.jello_app.model.Role;
+import com.jello.jello_app.repository.RoleRepository;
+import com.jello.jello_app.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Transactional
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationListener<ApplicationReadyEvent> {
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Set<String> defaultRoles = Set.of("ROLE_ADMIN", "ROLE_USER");
+        createDefaultRoles(defaultRoles);
+        createDefaultUsers();
+        createDefaultAdmins();
+    }
+
+    private void createDefaultRoles(Set<String> roles) {
+        roles.forEach(role -> roleRepository.findByName(role)
+                .orElseGet(() -> {
+                    Role newRole = new Role(role);
+                    roleRepository.save(newRole);
+                    System.out.println("Created role: " + newRole);
+                    return newRole;
+                }));
+    }
+}
