f1e0837cc16fa1f3c47b2cae59e0331043396b1c
jupitters <sampaiobale@gmail.com>
2026-04-08 20:20:16 -0300

    app user details

--- src/main/java/com/jello/jello_app/security/user/AppUserDetails.java
+++ src/main/java/com/jello/jello_app/security/user/AppUserDetails.java
@@ -1,7 +1,9 @@
 package com.jello.jello_app.security.user;
 
 import com.jello.jello_app.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,9 +14,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppUserDetails implements UserDetails {
     private Long id;
     private String email;
+    private String username;
     private String password;
 
     private Collection<GrantedAuthority> authorities;
@@ -36,16 +41,36 @@ public class AppUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return authorities;
     }
 
     @Override
     public @Nullable String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
