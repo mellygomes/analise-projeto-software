20efba57d5a99d326d34b5653d3a979edb1152cd
jupitters <sampaiobale@gmail.com>
2026-04-08 18:52:31 -0300

    App user details

---
+++ src/main/java/com/jello/jello_app/security/user/AppUserDetails.java
@@ -0,0 +1,49 @@
+package com.jello.jello_app.security.user;
+
+import lombok.Data;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class AppUserDetails implements UserDetails {
+    private Long id;
+    private String email;
+    private String password;
+
+    private Collection<GrantedAuthority> authorities;
+
+    public static AppUserDetails buildUserDetails(User user) {
+        List<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new AppUserDetails(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
