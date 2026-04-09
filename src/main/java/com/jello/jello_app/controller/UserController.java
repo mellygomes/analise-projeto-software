9445a089ce7d5668271edd586f0477293dac5b78
jupitters <sampaiobale@gmail.com>
2026-04-09 19:24:11 -0300

    controller get user by id

--- src/main/java/com/jello/jello_app/controller/UserController.java
+++ src/main/java/com/jello/jello_app/controller/UserController.java
@@ -8,10 +8,7 @@ import com.jello.jello_app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
+    @PostMapping("/register")
     public ResponseEntity<ApiResponse> register(@RequestBody RegisterRequest request) {
         try {
             User user = userService.register(request);
@@ -30,6 +28,7 @@ public class UserController {
         }
     }
 
+    @GetMapping("/{userId}")
     public ResponseEntity<ApiResponse> getUserById(@PathVariable Long userId){
         try {
             User user = userService.getUserById(userId);
