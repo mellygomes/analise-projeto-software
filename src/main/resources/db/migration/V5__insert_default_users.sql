d5468372bc6fabe2ce118ff8cf6910b3a93ac2f1
jupitters <sampaiobale@gmail.com>
2026-04-28 10:38:57 -0300

    close #5

---
+++ src/main/resources/db/migration/V5__insert_default_users.sql
@@ -0,0 +1,9 @@
+INSERT INTO users (email, first_name, last_name, password, username, profile_picture)
+VALUES ('ana@dev.com', 'Ana', 'Dev', '$2a$10$2JT8fttnQZr6JdvE2QP42OGUIglYcOCf5EeEHIpdt/UBPZPXAw2a6', 'aninhadev', null);
+INSERT INTO users (email, first_name, last_name, password, username, profile_picture)
+VALUES ('bruno@db.com', 'Bruno', 'Db', '$2a$10$2JT8fttnQZr6JdvE2QP42OGUIglYcOCf5EeEHIpdt/UBPZPXAw2a6', 'debenelson', null);
+INSERT INTO users (email, first_name, last_name, password, username, profile_picture)
+VALUES ('carlos@marketing.com', 'Carlos', 'Marketing', '$2a$10$2JT8fttnQZr6JdvE2QP42OGUIglYcOCf5EeEHIpdt/UBPZPXAw2a6', 'carlosmarques', null);
+INSERT INTO user_roles (role_id, user_id) VALUES (2, 1);
+INSERT INTO user_roles (role_id, user_id) VALUES (2, 2);
+INSERT INTO user_roles (role_id, user_id) VALUES (2, 3);
