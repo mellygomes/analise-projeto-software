6832c9e2af2386173176646568d1841d0d77f086
jupitters <sampaiobale@gmail.com>
2026-04-28 22:28:32 -0300

    image model

--- src/main/java/com/jello/jello_app/repository/ImageRepository.java
+++ src/main/java/com/jello/jello_app/repository/ImageRepository.java
@@ -1,4 +1,7 @@
 package com.jello.jello_app.repository;
 
-public interface ImageRepository {
+import com.jello.jello_app.model.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
 }
---
+++ src/main/java/com/jello/jello_app/service/ImageService.java
@@ -0,0 +1,9 @@
+package com.jello.jello_app.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ImageService {
+
+}
