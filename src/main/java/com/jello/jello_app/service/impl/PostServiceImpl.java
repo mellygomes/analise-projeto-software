becbe6b17349d8c6550a0062699dfe9cb23f5342
jupitters <sampaiobale@gmail.com>
2026-05-04 22:26:26 -0300

    post service create post

--- src/main/java/com/jello/jello_app/service/impl/PostServiceImpl.java
+++ src/main/java/com/jello/jello_app/service/impl/PostServiceImpl.java
@@ -2,8 +2,10 @@ package com.jello.jello_app.service.impl;
 
 import com.jello.jello_app.dto.CreatePostRequest;
 import com.jello.jello_app.model.Post;
+import com.jello.jello_app.model.User;
 import com.jello.jello_app.service.ImageService;
 import com.jello.jello_app.service.PostService;
+import com.jello.jello_app.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
     private final ImageService imageService;
+    private final UserService userService;
+    private final PostRepository postRepository;
 
     @Override
     @Transactional
     public Post createPost(CreatePostRequest request) {
+        User user = userService.getAuthenticatedUser();
+
+        Post post = new Post();
+        post.setTitle(request.getTitle());
+        post.setContent(request.getContent());
+        post.setUser(user);
+
+        Post savedPost = postRepository.save(post);
+
         return null;
     }
 }
