b85a5068a2e0d7704dabd605c27bf87619b023ce
jupitters <sampaiobale@gmail.com>
2026-05-04 22:50:54 -0300

    remove controller to save single image

--- src/main/java/com/jello/jello_app/controller/PostController.java
+++ src/main/java/com/jello/jello_app/controller/PostController.java
@@ -7,10 +7,7 @@ import com.jello.jello_app.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -21,6 +18,7 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
 
+    @PostMapping("/create")
     public ResponseEntity<ApiResponse> createPost(@RequestParam List<MultipartFile> images, @RequestBody CreatePostRequest request) {
         try {
             Post createdPost = postService.createPost(request, images);
