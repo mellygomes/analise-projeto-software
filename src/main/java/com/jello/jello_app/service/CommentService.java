e8fab3c9f8c484b9467ff4f431d8a8b6cc398ca8
jupitters <sampaiobale@gmail.com>
2026-05-05 14:46:08 -0300

    comment service get all comments from post

--- src/main/java/com/jello/jello_app/service/CommentService.java
+++ src/main/java/com/jello/jello_app/service/CommentService.java
@@ -3,7 +3,10 @@ package com.jello.jello_app.service;
 import com.jello.jello_app.dto.CommentDTO;
 import com.jello.jello_app.model.Comment;
 
+import java.util.List;
+
 public interface CommentService {
     Comment addComment(String comment, Long postId);
     CommentDTO commentDTOBuilder(Comment comment);
+    List<CommentDTO> getCommentsFromPost(Long postId);
 }
