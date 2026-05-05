e8156bd8c405f02cd30a70653ae759bda5db4784
jupitters <sampaiobale@gmail.com>
2026-05-05 00:53:18 -0300

    foreign keys

--- src/main/resources/db/migration/V9__create_comments.sql
+++ src/main/resources/db/migration/V9__create_comments.sql
@@ -4,6 +4,6 @@ CREATE TABLE public.comments
     post_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
     content VARCHAR(255),
-    CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    CONSTRAINT fk_comments_post FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_comments_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
