f5c1c5ff5892ce574d827bb97f6f7f56abc8dd43
jupitters <sampaiobale@gmail.com>
2026-05-05 14:45:36 -0300

    comment repo

--- src/main/java/com/jello/jello_app/repository/CommentRepository.java
+++ src/main/java/com/jello/jello_app/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.jello.jello_app.repository;
 import com.jello.jello_app.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Long postId);
 }
