470df838ef5cba73acab49028bb7d26217ef20e3
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-05-06 00:02:28 -0300

    add follows entity and migration

---
+++ src/main/java/com/jello/jello_app/model/Follow.java
@@ -0,0 +1,37 @@
+package com.jello.jello_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "follows",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"follower_id", "following_id"})
+        }
+)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User following;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
