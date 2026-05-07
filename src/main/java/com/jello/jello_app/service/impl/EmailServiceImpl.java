11ebe603065cf4e428c4ebe00c393ed4a184d160
jupitters <sampaiobale@gmail.com>
2026-05-07 14:52:50 -0300

    send new email account

--- src/main/java/com/jello/jello_app/service/impl/EmailServiceImpl.java
+++ src/main/java/com/jello/jello_app/service/impl/EmailServiceImpl.java
@@ -2,13 +2,18 @@ package com.jello.jello_app.service.impl;
 
 import com.jello.jello_app.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService {
+    public static final String NEW_USER_ACCOUNT_VERIFICATION = "New user account verification";
+
     private final JavaMailSender sender;
     @Value("${spring.mail.verify.host}")
     private String host;
@@ -18,7 +23,16 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendNewAccountEmail(String name, String emailTo, String token) {
-        SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
+            message.setFrom(fromMail);
+            message.setTo(emailTo);
+            message.setText(getEmailMessage(name, host, token));
+            sender.send(message);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 
     @Override
