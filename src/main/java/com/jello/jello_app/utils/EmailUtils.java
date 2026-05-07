c107e1118672fbd1ad7d12469c1d9154e0e3a948
jupitters <sampaiobale@gmail.com>
2026-05-07 15:33:23 -0300

    email utils reset password

--- src/main/java/com/jello/jello_app/utils/EmailUtils.java
+++ src/main/java/com/jello/jello_app/utils/EmailUtils.java
@@ -10,7 +10,19 @@ public class EmailUtils {
                 "\n\nThe Support Team";
     }
 
+    public static String getResetPasswordMessage(String name, String host, String token) {
+        return "Hello" + name + ",\n\n" +
+                "Your new account has been created. " +
+                "Please click on the link below to verify your account.\n\n" +
+                getResetPasswordUrl(host, token) +
+                "\n\nThe Support Team";
+    }
+
     public static String getVerificationUrl(String host, String token) {
         return host + "/verify/account?token=" + token;
+    }
+
+    public static String getResetPasswordUrl(String host, String token) {
+        return host + "/verify/password?token=" + token;
     }
 }
