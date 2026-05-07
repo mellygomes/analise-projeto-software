baea70a0b8adba5dd48ff34c0d8ce0f5da7b7697
jupitters <sampaiobale@gmail.com>
2026-05-07 15:15:10 -0300

    send password request

--- src/main/java/com/jello/jello_app/service/impl/EmailServiceImpl.java
+++ src/main/java/com/jello/jello_app/service/impl/EmailServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class EmailServiceImpl implements EmailService {
     public static final String NEW_USER_ACCOUNT_VERIFICATION = "New user account verification";
+    public static final String PASSSWORD_RESET_REQUEST = "Reset password request";
 
     private final JavaMailSender sender;
     @Value("${spring.mail.verify.host}")
@@ -37,6 +38,15 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendPasswordResetEmail(String name, String emailTo, String token) {
-
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject(PASSSWORD_RESET_REQUEST);
+            message.setFrom(fromMail);
+            message.setTo(emailTo);
+            message.setText(getResetPasswordMessage(name, host, token));
+            sender.send(message);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
