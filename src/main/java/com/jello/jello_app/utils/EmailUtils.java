6f2dfbe49f29649d49823554725e9cdd73922ae3
jupitters <sampaiobale@gmail.com>
2026-05-07 15:59:35 -0300

    add files

---
+++ src/main/java/com/jello/jello_app/utils/EmailUtils.java
@@ -0,0 +1,27 @@
+package com.jello.jello_app.utils;
+
+public class EmailUtils {
+    public static String getEmailMessage(String name, String host, String token) {
+        return "Hello" + name + ",\n\n" +
+                "Your new account has been created. " +
+                "Please click on the link below to verify your account.\n\n" +
+                getVerificationUrl(host, token) +
+                "\n\nThe Support Team";
+    }
+
+    public static String getResetPasswordMessage(String name, String host, String token) {
+        return "Hello" + name + ",\n\n" +
+                "Your new account has been created. " +
+                "Please click on the link below to verify your account.\n\n" +
+                getResetPasswordUrl(host, token) +
+                "\n\nThe Support Team";
+    }
+
+    public static String getVerificationUrl(String host, String token) {
+        return host + "/verify/account?token=" + token;
+    }
+
+    public static String getResetPasswordUrl(String host, String token) {
+        return host + "/verify/password?token=" + token;
+    }
+}
