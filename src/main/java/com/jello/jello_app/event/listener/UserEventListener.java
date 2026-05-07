5805e9f5e603f9c4b7f0f3d4288df354f4c30511
jupitters <sampaiobale@gmail.com>
2026-05-07 16:14:33 -0300

    user event listener

--- src/main/java/com/jello/jello_app/event/listener/UserEventListener.java
+++ src/main/java/com/jello/jello_app/event/listener/UserEventListener.java
@@ -1,4 +1,23 @@
 package com.jello.jello_app.event.listener;
 
+import com.jello.jello_app.event.UserEvent;
+import com.jello.jello_app.service.EmailService;
+import com.jello.jello_app.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class UserEventListener {
+    private final EmailService emailService;
+
+    @EventListener
+    public void onUserEvent(UserEvent event) {
+        switch (event.getType()) {
+            case REGISTRATION -> emailService.sendNewAccountEmail(event.getUser().getUsername(), event.getUser().getEmail(), (String)event.getData().get("key"));
+            case RESET_PASSWORD -> emailService.sendPasswordResetEmail(event.getUser().getUsername(), event.getUser().getEmail(), (String)event.getData().get("key"));
+            default -> {}
+        }
+    }
 }
