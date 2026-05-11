c84e23aabe657f019beea1bbebec442aff83bb81
jupitters <sampaiobale@gmail.com>
2026-05-11 13:47:08 -0300

    confirmation

--- src/main/java/com/jello/jello_app/model/Confirmation.java
+++ src/main/java/com/jello/jello_app/model/Confirmation.java
@@ -1,9 +1,29 @@
 package com.jello.jello_app.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.UUID;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
 public class Confirmation extends Auditable {
+    private String key;
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    public Confirmation(User user) {
+        this.user = user;
+        this.key = UUID.randomUUID().toString();
+    }
 }
