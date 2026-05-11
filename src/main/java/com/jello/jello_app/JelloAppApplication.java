a2c2d120b186c2bed77769113a3b478a8c25f6cf
jupitters <sampaiobale@gmail.com>
2026-05-10 23:47:24 -0300

    enable async

--- src/main/java/com/jello/jello_app/JelloAppApplication.java
+++ src/main/java/com/jello/jello_app/JelloAppApplication.java
@@ -2,8 +2,10 @@ package com.jello.jello_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class JelloAppApplication {
 
 	public static void main(String[] args) {
