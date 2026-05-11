9a3d84fdb8cce930b4fe2793cb6ffc78fc7efd79
jupitters <sampaiobale@gmail.com>
2026-05-11 14:02:14 -0300

    confirmation

--- src/main/resources/db/migration/V10__create_confirmation.sql
+++ src/main/resources/db/migration/V10__create_confirmation.sql
@@ -1,6 +1,19 @@
-CREATE TABLE public.confirmation
+CREATE SEQUENCE IF NOT EXISTS primary_key_seq
+START WITH 1
+INCREMENT BY 1;
+
+CREATE TABLE public.confirmations
 (
-    confirmation_key VARCHAR NOT NULL UNIQUE,
-    user_id BIGINT NOT NULL,
-    CONSTRAINT fk_confirmation_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    id BIGINT NOT NULL PRIMARY KEY DEFAULT nextval('primary_key_seq'),
+    reference_id VARCHAR(255),
+    created_by BIGINT NOT NULL,
+    updated_by BIGINT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    confirmation_key VARCHAR(255) NOT NULL UNIQUE,
+    user_id BIGINT NOT NULL UNIQUE,
+    CONSTRAINT fk_confirmation_user
+        FOREIGN KEY (user_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
 );
