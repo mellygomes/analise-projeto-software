4ccb3b1194cbdd8239b5a9b85a77c8116254f8ff
jupitters <sampaiobale@gmail.com>
2026-05-12 15:45:02 -0300

    confirmation repo

--- src/main/java/com/jello/jello_app/repository/ConfirmationRepository.java
+++ src/main/java/com/jello/jello_app/repository/ConfirmationRepository.java
@@ -1,7 +1,10 @@
 package com.jello.jello_app.repository;
 
 import com.jello.jello_app.model.Confirmation;
+import com.jello.jello_app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConfirmationRepository extends JpaRepository<Confirmation, Long> {
+    Confirmation findByConfirmationKey(String key);
+    Confirmation findByUser(User user);
 }
