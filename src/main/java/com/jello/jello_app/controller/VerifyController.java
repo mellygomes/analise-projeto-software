92400105d429f96ee3893a3f7e6d9f673a543b70
jupitters <sampaiobale@gmail.com>
2026-05-12 15:24:31 -0300

    verify controller

---
+++ src/main/java/com/jello/jello_app/controller/VerifyController.java
@@ -0,0 +1,22 @@
+package com.jello.jello_app.controller;
+
+import com.jello.jello_app.dto.ApiResponse;
+import com.jello.jello_app.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/verify")
+public class VerifyController {
+    private final UserService userService;
+
+    public ResponseEntity<ApiResponse> verifyAccount(@RequestParam String token) {
+        userService.verifyAccountKey(token);
+        return ResponseEntity.ok().body(new ApiResponse("Account verified!", null));
+    }
+}
