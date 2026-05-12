ba114504a84b2e9eed54eeb6e2a5a680538d4fed
jupitters <sampaiobale@gmail.com>
2026-05-11 23:18:55 -0300

    email utils

--- src/main/java/com/jello/jello_app/utils/EmailUtils.java
+++ src/main/java/com/jello/jello_app/utils/EmailUtils.java
@@ -3,8 +3,8 @@ package com.jello.jello_app.utils;
 public class EmailUtils {
     public static String getEmailMessage(String name, String host, String token) {
         return "Hello, @" + name + "\n\n" +
-                "Your new account has been created. " +
-                "Please click on the link below to verify your account.\n\n" +
+                "Your new account has been created! " +
+                "Please, click on the link below to verify your account.\n\n" +
                 getVerificationUrl(host, token) +
                 "\n\nThe Support Team";
     }
