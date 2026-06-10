0838cba255c89f30d44d72c16f7e47ef010393bd
jupitters <sampaiobale@gmail.com>
2026-06-10 11:08:15 -0300

    fix confirmation

--- src/main/resources/db/migration/V11__create_confirmations.sql
+++ src/main/resources/db/migration/V11__create_confirmations.sql
@@ -9,5 +9,6 @@ CREATE TABLE public.confirmations
     updated_at TIMESTAMP(6) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     confirmation_key VARCHAR(255) NOT NULL UNIQUE,
     CONSTRAINT fk_confirmation_user_created_by FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk_confirmation_user_updated_by FOREIGN KEY (updated_by) REFERENCES users(id) ON DELETE CASCADE
+    CONSTRAINT fk_confirmation_user_updated_by FOREIGN KEY (updated_by) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_confirmation_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
 );
