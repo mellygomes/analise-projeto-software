2d361d348965859a47bff47f0d1b8f49cbc5738e
jupitters <sampaiobale@gmail.com>
2026-06-11 13:09:53 -0300

    old tables

--- src/main/resources/db/migration/V11__create_confirmations.sql
+++ src/main/resources/db/migration/V11__create_confirmations.sql
@@ -1,7 +1,7 @@
 CREATE TABLE public.confirmations
 (
     id BIGINT NOT NULL PRIMARY KEY DEFAULT nextval('primary_key_seq'),
-    user_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL UNIQUE,
     confirmation_key VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP(6) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_confirmation_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
--- src/main/resources/db/migration/V9__create_comments.sql
+++ src/main/resources/db/migration/V9__create_comments.sql
@@ -7,8 +7,10 @@ CREATE TABLE public.comments
     created_at TIMESTAMP(6) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP(6) WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     post_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     content VARCHAR(255),
     CONSTRAINT fk_comments_post FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
+    CONSTRAINT fk_comments_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_comments_user_created_by FOREIGN KEY (created_by) REFERENCES users(id) ON DELETE CASCADE,
     CONSTRAINT fk_comments_user_updated_by FOREIGN KEY (updated_by) REFERENCES users(id) ON DELETE CASCADE
 );
