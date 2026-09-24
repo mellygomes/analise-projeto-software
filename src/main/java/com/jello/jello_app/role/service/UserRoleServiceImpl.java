1b27a9a05b25cd21520178837c6538ae8963a4d3
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-09-24 12:51:37 -0300

    refactor: update role management methods to not overwrite existing user roles

--- src/main/java/com/jello/jello_app/role/service/UserRoleServiceImpl.java
+++ src/main/java/com/jello/jello_app/role/service/UserRoleServiceImpl.java
@@ -7,9 +7,6 @@ import com.jello.jello_app.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Service
 @RequiredArgsConstructor
 public class UserRoleServiceImpl implements UserRoleService {
@@ -17,14 +14,13 @@ public class UserRoleServiceImpl implements UserRoleService {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
 
-
     @Override
     public User grantAdmin(Long userId) {
         Role adminRole = roleRepository.findByName("ROLE_ADMIN")
                 .orElseThrow(() -> new RuntimeException("ROLE_ADMIN não encontrado!"));
         return userRepository.findById(userId)
                 .map(existingUser -> {
-                    existingUser.setRoles(new HashSet<>(Set.of(adminRole)));
+                    existingUser.getRoles().add(adminRole);
                     return userRepository.save(existingUser);
                 })
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
@@ -32,14 +28,19 @@ public class UserRoleServiceImpl implements UserRoleService {
 
     @Override
     public User revokeAdmin(Long userId) {
-        Role userRole = roleRepository.findByName("ROLE_USER")
-                .orElseThrow(() -> new RuntimeException("ROLE_USER não encontrado!"));
-        return userRepository.findById(userId)
-                .map(existingUser -> {
-                    existingUser.setRoles(new HashSet<>(Set.of(userRole)));
-                    return userRepository.save(existingUser);
-                })
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+        Role adminRole = roleRepository.findByName("ROLE_ADMIN")
+                .orElseThrow(() -> new RuntimeException("ROLE_ADMIN não encontrado!"));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuário nao encontrado"));
+
+        boolean removedAdmin = user.getRoles().removeIf(role -> role.getName().equals(adminRole.getName()));
+
+        if (!removedAdmin) {
+            throw new RuntimeException("Usuário não possui o cargo de ADMIN!");
+        }
+
+        return userRepository.save(user);
     }
 
     @Override
@@ -48,7 +49,7 @@ public class UserRoleServiceImpl implements UserRoleService {
                 .orElseThrow(() -> new RuntimeException("ROLE_MODERATOR não encontrado!"));
         return userRepository.findById(userId)
                 .map(existingUser -> {
-                    existingUser.setRoles(new HashSet<>(Set.of(moderatorRole)));
+                    existingUser.getRoles().add(moderatorRole);
                     return userRepository.save(existingUser);
                 })
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
@@ -56,13 +57,18 @@ public class UserRoleServiceImpl implements UserRoleService {
 
     @Override
     public User revokeModerator(Long userId) {
-        Role userRole = roleRepository.findByName("ROLE_USER")
-                .orElseThrow(() -> new RuntimeException("ROLE_USER não encontrado!"));
-        return userRepository.findById(userId)
-                .map(existingUser -> {
-                    existingUser.setRoles(new HashSet<>(Set.of(userRole)));
-                    return userRepository.save(existingUser);
-                })
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
+        Role moderatorRole = roleRepository.findByName("ROLE_MODERATOR")
+                .orElseThrow(() -> new RuntimeException("ROLE_MODERATOR não encontrado!"));
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        boolean removedModerator = user.getRoles().removeIf(role -> role.getName().equals(moderatorRole.getName()));
+
+        if (!removedModerator) {
+            throw new RuntimeException("Usuário nao possui o cargo de MODERADOR!");
+        }
+
+        return userRepository.save(user);
     }
 }
